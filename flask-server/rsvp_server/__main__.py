55bafd2443db1ca1f92a567d83ff3562ae59a140
Aaron Lauterer <aaron@lauterer.at>
2018-02-23 20:07:14 +0100

    Clean early SQLAlchemy test

--- flask-server/rsvp_server/__main__.py
+++ flask-server/rsvp_server/__main__.py
@@ -11,22 +11,9 @@ def main():
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'RSVP API'})
 
-    from flask_sqlalchemy import SQLAlchemy
 
-    app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-    db = SQLAlchemy(app.app)
-
-    class User(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(80), unique=True, nullable=False)
-        email = db.Column(db.String(120), unique=True, nullable=False)
-
-        def __repr__(self):
-            return '<User %r>' % self.username
 
     app.run(port=8888)
-
-
 
 if __name__ == '__main__':
     main()
